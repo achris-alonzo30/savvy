1718bb04540d3dc07b055680b539aee5bd678af6
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-27 10:20:55 -0300

    create migration folder and script

---
+++ apps/web/scripts/migrate.ts
@@ -0,0 +1,22 @@
+
+import { config } from "dotenv";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+
+config({ path: ".env.local" });
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
+
+async function run() {
+    try {
+        await migrate(db, { migrationsFolder: "drizzle"})
+    } catch (e) {
+        console.error("Error during migration: ", e);
+        process.exit(1);
+    }
+}
+
+run();
+
