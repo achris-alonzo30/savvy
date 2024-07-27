54eea647fbb2b4360772127fdfbe6190d25947b7
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-27 17:54:33 -0300

    add pagination and sorting functionality

--- apps/web/components/data-table.tsx
+++ apps/web/components/data-table.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { useState } from "react"
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
-  useReactTable,
   getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
 } from "@tanstack/react-table"
 
 import {
@@ -27,12 +30,20 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-  })
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
 
   return (
     <section>
