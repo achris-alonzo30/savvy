8c518a570c7a8f5e9df9ec68090d124f574e94fc
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-27 19:02:16 -0300

    create get specific account API request

--- apps/web/app/api/[[...route]]/accounts.ts
+++ apps/web/app/api/[[...route]]/accounts.ts
@@ -24,7 +24,38 @@ const app = new Hono()
                 .where(eq(accounts.userId, auth.userId));
 
             return c.json({ data });
-        })
+    })
+    .get("/:id",
+        clerkMiddleware(),
+        zValidator("param", z.object({
+            id: z.string().optional(),
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const { id } = c.req.valid("param");
+
+            if (!id) return c.json({ error: "Missing id" }, 400);
+
+            if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+            const [data] = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name
+                })
+                .from(accounts)
+                .where(
+                    and(
+                        eq(accounts.id, id),
+                        eq(accounts.userId, auth.userId)
+                    )
+                );
+            
+            if (!data) return c.json({ error: "Not found" }, 404);
+
+            return c.json({ data });
+        }
+    )
     .post("/",
         clerkMiddleware(),
         zValidator("json", insertAccountSchema.pick({
@@ -77,5 +108,6 @@ const app = new Hono()
             return c.json({ data });
         }
     )
+
 
 export default app;
