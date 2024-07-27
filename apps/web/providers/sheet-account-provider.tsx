7c53eef79a5a755b3eb1d26bdbeacdc5b299e67d
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-27 16:22:46 -0300

    create SheetAccountProvider

---
+++ apps/web/providers/sheet-account-provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { NewAccountSheet } from "@/components/accounts/new-account-sheet";
+
+export const SheetProvider = () => {
+    return (
+        <>
+            <NewAccountSheet />
+        </>
+
+    )
+}
