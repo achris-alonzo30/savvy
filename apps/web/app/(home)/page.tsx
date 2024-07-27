31b0a0591ea99251ec5334bb0072d7d6a638acd8
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-27 17:42:05 -0300

    add components inside the account page

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,14 +1,9 @@
-"use client";
 
-import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/hooks/use-new-account";
 
 const HomePage = () => {
-    const { onOpen, onClose} = useNewAccount();
     return (
-        <Button onClick={onOpen}>
-            Open Account
-        </Button>
+        <></>
     )
 }
 
--- apps/web/app/accounts/page.tsx
+++ apps/web/app/accounts/page.tsx
@@ -1,6 +1,35 @@
+"use client";
+
+import { 
+    Card,
+    CardTitle,
+    CardHeader,
+    CardContent,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+import { useNewAccount } from "@/hooks/use-new-account";
+import { Plus } from "lucide-react";
+
 const AccountPage = () => {
+    const { onOpen } = useNewAccount();
     return (
-        <></>
+        <main className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts
+                    </CardTitle>
+                    <Button 
+                        size="sm"
+                        onClick={onOpen}
+                    >
+                        <Plus className="size-4 mr-2" />
+                        Add New
+                    </Button>
+                </CardHeader>
+            </Card>
+        </main>
     )
 }
 
